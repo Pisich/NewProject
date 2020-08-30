ee546ec1344952cd89a9a37bf26300ce5fcb0142
Carlos Rodriguez <croc1110@hotmail.com>
2020-08-30 12:40:14 -0500

    Update main.c
    Added some initial prints for the program

--- main.c
+++ main.c
@@ -1,2 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void initial_print(){
+    printf("Welcome to PisiCh Bank!\n");
+    printf("What operation would you like to perform?\n");
+}
+
+
 int main(){
+    initial_print();
 }
