3d70bcdb6545292b303d31653a3b1c8c41d4adb9
Pisich <62033118+Pisich@users.noreply.github.com>
2020-08-30 13:41:43 -0500

    Update main.c
    Added new structs for Accounts and LoggedUsers

--- main.c
+++ main.c
@@ -3,6 +3,19 @@
 #include <string.h>
 #include <unistd.h>
 
+typedef struct Account{
+    int number[19];
+    char name[100];
+    long float balance;
+    short typeAcc;
+}Account;
+
+typedef struct LoggedUser{
+    char user[100];
+    char password[100];
+    short perms;
+}LoggedUser;
+
 void initial_print(){
     system("figlet -c PisiCh Bank");
     printf("What operation would you like to perform?\n");
