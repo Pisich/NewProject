45abf5f6f97ff52636395169f16ef8f2a36e0005
Carlos Rodriguez <croc1110@hotmail.com>
2020-08-30 14:45:03 -0500

    Update main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 typedef struct Account{
     int number[19];
     char name[100];
-    long float balance;
+    long double balance;
     short typeAcc;
 }Account;
 
@@ -17,11 +17,21 @@ typedef struct LoggedUser{
 }LoggedUser;
 
 void initial_print(){
-    system("figlet -c PisiCh Bank");
+    //system("figlet -c PisiCh Bank");
     printf("What operation would you like to perform?\n");
+    printf("1. Create a new account\n2. Check the balance of your account\n");
+    printf("3. Deposit money into your account\n4. Withdraw money from your account\n");
+    printf("5. Change any account details\n6. Delete an existing account\n");
+    printf("Enter the number of your selection: ");
 }
 
 
 int main(){
+    int test = 12;
+    int *selection = &test;
     initial_print();
+    scanf("%d", selection);
+
+    printf("%d\n", *selection);
+
 }
