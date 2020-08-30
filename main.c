1d38280546eaa7b612f052f000320a133d533b13
Carlos Rodriguez <croc1110@hotmail.com>
2020-08-30 15:06:13 -0500

    Update main.c
    Added test functions for every operation and user input checks.

--- main.c
+++ main.c
@@ -24,14 +24,49 @@ void initial_print(){
     printf("5. Change any account details\n6. Delete an existing account\n");
     printf("Enter the number of your selection: ");
 }
+void create_acc(){
+  printf("create acc\n");
+}
 
+void check_bal(){
+  printf("check bal\n");
+}
+
+void deposit(){
+  printf("deposit\n");
+}
+
+void withdraw(){
+  printf("withdraw\n");
+}
+
+void change_details(){
+  printf("change det\n");
+}
+
+void delete_acc(){
+  printf("del acc\n");
+}
 
 int main(){
     int test = 12;
     int *selection = &test;
     initial_print();
     scanf("%d", selection);
-
-    printf("%d\n", *selection);
-
+    while(*selection >= 7 || *selection <= 0){
+      printf("Invalid selection, enter a valid operation: ");
+      scanf("%d", selection);
+    }
+    if(*selection == 1)
+      create_acc();
+    else if(*selection == 2)
+      check_bal();
+    else if(*selection == 3)
+      deposit();
+    else if(*selection == 4)
+      withdraw();
+    else if(*selection == 5)
+      change_details();
+    else if(*selection == 6)
+      delete_acc();
 }
