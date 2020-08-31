d5f659e9695e456a73fee028036e5a1f763bd1e1
Carlos Rodriguez <croc1110@hotmail.com>
2020-08-31 14:34:38 -0500

    Update main.c
    Fixed full name input

--- main.c
+++ main.c
@@ -3,35 +3,57 @@
 #include <string.h>
 #include <unistd.h>
 
+#define MAX_NAME_LEN 100
+#define MAX_PASS_LEN 50
+
 typedef struct Account{
-    int number[19];
-    char name[100];
-    long double balance;
-    short typeAcc;
-    int PIN;
+  int number[19];
+  char name[MAX_NAME_LEN];
+  long double balance;
+  short typeAcc;
+  int PIN;
 }Account;
 
 typedef struct LoggedUser{
-    char user[100];
-    char password[100];
-    short perms;
-    int details;
+  char user[MAX_NAME_LEN];
+  char password[MAX_PASS_LEN];
+  short perms;
+  int details;
 }LoggedUser;
 
 void initial_print(){
-    //system("figlet -c PisiCh Bank");
-    printf("What operation would you like to perform?\n");
-    printf("1. Create a new account\n2. Check the balance of your account\n");
-    printf("3. Deposit money into your account\n4. Withdraw money from your account\n");
-    printf("5. Change any account details\n6. Delete an existing account\n");
-    printf("Enter the number of your selection: ");
+  //system("figlet -c PisiCh Bank");
+  printf("What operation would you like to perform?\n");
+  printf("1. Create a new account\n2. Check the balance of your account\n");
+  printf("3. Deposit money into your account\n4. Withdraw money from your account\n");
+  printf("5. Change any account details\n6. Delete an existing account\n");
+  printf("Enter the number of your selection: ");
 }
+
+void create_savings(char *name){
+  //system("figlet -c Savings");
+  struct Account newAccount;
+  newAccount.typeAcc = 1;
+  for(int i = 0; i < MAX_NAME_LEN; i++)
+    newAccount.name[i] = *(name + i);
+  printf("%s", newAccount.name);
+}
+void create_checks(char *name){
+  //system("figlet -c Checks");
+  struct Account newAccount;
+  newAccount.typeAcc = 2;
+  for(int i = 0; i < MAX_NAME_LEN; i++)
+    newAccount.name[i] = *(name + i);
+  printf("%s", newAccount.name);
+}
+
 void create_acc(){
   int test = 0;
   int *age = &test;
   int acc;
-  char name[100];
-  //system("figlet Create new account");
+  char temp;
+  char name[MAX_NAME_LEN];
+  //system("figlet -c Create new account");
   printf("Enter your age: ");
   scanf("%d", age);
   while(*age <= 0){
@@ -42,8 +64,9 @@ void create_acc(){
     printf("You are not allowed to create a bank account if you are under 18 years old, come back in %d years.\n", 18 - *age);
     exit(0);
   }
+  scanf("%c", &temp);
   printf("Enter your full name: ");
-  scanf(" %[^\t\n]s",name);
+  scanf("%[^\n]s", name);
   printf("What kind of account would you like to open?\n");
   printf("1. Savings\n2. Checks\n");
   printf("Enter your selection: ");
@@ -53,9 +76,9 @@ void create_acc(){
     scanf("%d", &acc);
   }
   if(acc == 1)
-    //system("figlet -c Savings");
+    create_savings(name);
   else if(acc == 2)
-    //system("figlet -c Checks");
+    create_checks(name);
 }
 
 void check_bal(){
@@ -79,24 +102,24 @@ void delete_acc(){
 }
 
 int main(){
-    int test = 12;
-    int *selection = &test;
-    initial_print();
+  int test = 12;
+  int *selection = &test;
+  initial_print();
+  scanf("%d", selection);
+  while(*selection >= 7 || *selection <= 0){
+    printf("Invalid selection, enter a valid operation: ");
     scanf("%d", selection);
-    while(*selection >= 7 || *selection <= 0){
-      printf("Invalid selection, enter a valid operation: ");
-      scanf("%d", selection);
-    }
-    if(*selection == 1)
-      create_acc();
-    else if(*selection == 2)
-      check_bal();
-    else if(*selection == 3)
-      deposit();
-    else if(*selection == 4)
-      withdraw();
-    else if(*selection == 5)
-      change_details();
-    else if(*selection == 6)
-      delete_acc();
+  }
+  if(*selection == 1)
+    create_acc();
+  else if(*selection == 2)
+    check_bal();
+  else if(*selection == 3)
+    deposit();
+  else if(*selection == 4)
+    withdraw();
+  else if(*selection == 5)
+    change_details();
+  else if(*selection == 6)
+    delete_acc();
 }
