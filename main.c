96293d0371521f8c613efcbf636130f073822e36
Carlos Rodriguez <croc1110@hotmail.com>
2020-09-07 18:38:30 -0500

    When creating an acc, the 15 other numbers are also generated

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 
 struct Account{
   char acc_holder_name[MAX_LEN];
-  int acc_number;
+  int first_acc_number;
+  char acc_numbers[15];
   float available_balance;  
 };
 
@@ -28,7 +29,7 @@ void withdraw(){
   printf("\nEnter the amount of money you want to withdraw from this account ");
   scanf("%f",&withdraw_money);
  
-  if(loc_acc_num == accArr[loc_acc_num-1].acc_number){
+  if(loc_acc_num == accArr[loc_acc_num-1].first_acc_number){
     accArr[loc_acc_num-1].available_balance=
     accArr[loc_acc_num-1].available_balance-withdraw_money;
     printf("\nThe new balance of this account is %.2f \n", accArr[loc_acc_num-1].available_balance);
@@ -47,11 +48,10 @@ void deposit(){
   printf("\nEnter money you want to deposit:  ");
   scanf("%f",&add_money);
  
-  if (loc_acc_num == accArr[loc_acc_num-1].acc_number){
+  if (loc_acc_num == accArr[loc_acc_num-1].first_acc_number){
     accArr[loc_acc_num-1].available_balance=
     accArr[loc_acc_num-1].available_balance+add_money;
-    printf("\nThe New balance for account %d is %.2f \n", 
-    loc_acc_num, accArr[loc_acc_num-1].available_balance);
+    printf("\nThe new balance for this account is %.2f \n", accArr[loc_acc_num-1].available_balance);
    }
   loc_acc_num++;
 }
@@ -62,7 +62,7 @@ void acc_info(){
     printf("Account holder name: %s \n" , 
     accArr[num_acc].acc_holder_name);
     printf("Account number: %d \n" , 
-    accArr[num_acc].acc_number);
+    accArr[num_acc].first_acc_number);
     printf("Available balance: %.2f \n\n" , 
     accArr[num_acc].available_balance);
     num_acc++;
@@ -73,28 +73,32 @@ void create_account(){
   char acc_holder_name[MAX_LEN];
   char temp;
   char name[MAX_LEN];
-  int acc_number;
+  int acc_first_numb;
+  char other_acc_numbs[15];
   float available_balance = 0;
   fflush(stdin);
   printf("\nEnter the account holder name: ");
   scanf("%c", &temp);
   scanf("%[^\n]s", name);
-  printf("\nEnter your first account number(1 to 9): ");
-  scanf("%d", &acc_number);
+  printf("\nIn PisiChBank you can choose your first account number(1 to 9): ");
+  scanf("%d", &acc_first_numb);
+  for(int i = 0;i<15;i++) accArr[acc_first_numb-1].acc_numbers[i] = rand() % 10;
   scanf("%c", &temp);
-  strcpy(accArr[acc_number-1].acc_holder_name, acc_holder_name);
-   accArr[acc_number-1].acc_number = acc_number;
-   accArr[acc_number-1].available_balance = available_balance;
+
+  strcpy(accArr[acc_first_numb-1].acc_holder_name, acc_holder_name);
+   accArr[acc_first_numb-1].first_acc_number = acc_first_numb;
+   accArr[acc_first_numb-1].available_balance = available_balance;
+
   for(int i = 0; i < MAX_LEN; i++){
     if(name[i] >= 97 && name[i] <= 122 && name[i] != 32)
       name[i] = name[i] - 32;
   }
-  for(int i = 0; i < MAX_LEN; i++) accArr[acc_number-1].acc_holder_name[i] = name[i];
+  for(int i = 0; i < MAX_LEN; i++) accArr[acc_first_numb-1].acc_holder_name[i] = name[i];
   //system("figlet -c Account has been created successfully");
-  printf("Account holder name: %s \n" , accArr[acc_number-1].acc_holder_name);
+  printf("Account holder name: %s \n" , accArr[acc_first_numb-1].acc_holder_name);
 
-  printf("Account number: %d \n" , accArr[acc_number-1].acc_number);
-  printf("Available balance: %.2f \n" , accArr[acc_number-1].available_balance);
+  printf("Account number: %d \n" , accArr[acc_first_numb-1].first_acc_number);
+  printf("Available balance: %.2f \n" , accArr[acc_first_numb-1].available_balance);
 }
 
 void initial_print(){
